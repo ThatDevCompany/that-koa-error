5d62c38c2e9bd340b7ca2b8bb980b91912cd94cc
Steve James <steve@thatdevcompany.com>
2018-11-02 08:47:39 +1100

    Colourful Errors

--- src/utils/logApplicationError.ts
+++ src/utils/logApplicationError.ts
@@ -1,17 +1,44 @@
-import { ApplicationError } from '../errors'
+import { ApplicationError } from '@/errors'
+import * as colors from 'colors/safe'
+
+function colored(str: string) {
+	if (!str || str.length < 2) return ''
+	return colors.yellow(str)
+}
+
+function bold(str: string) {
+	if (!str || str.length < 2) return ''
+	return colors.bold(str)
+}
+
+function black(str: string) {
+	if (!str || str.length < 2) return ''
+	return colors.black(str)
+}
+
+function hr() {
+	return colored(bold('-----------------'))
+}
+
+function h1(str: string) {
+	if (!str || str.length < 2) return ''
+	return hr() + '\n' + colored(bold(str))
+}
+
+function h2(str: string) {
+	if (!str || str.length < 2) return ''
+	return hr() + '\n' + black(str)
+}
 
 export function logApplicationError(err: ApplicationError) {
-	console.warn('-----------------')
-	console.warn('APPLICATION ERROR')
-	console.warn('-----------------')
-	console.warn(err.status + ': ' + err.message)
-	if (err.debugMessage) {
-		console.warn(err.debugMessage)
-	}
-	if (err.debugInfo) {
-		console.warn(JSON.stringify(err.debugInfo, null, 4))
-	}
-	console.warn('-----------------')
-	console.warn(err)
-	console.warn('-----------------')
+	console.log(
+		h1('APPLICATION ERROR') +
+			'\n' +
+			colored(err.status + ': ' + err.message + '\n') +
+			h2(err.debugMessage + '\n') +
+			hr() +
+			'\n',
+		err.debugInfo,
+		'\n' + hr()
+	)
 }
--- src/utils/logUnexpectedError.ts
+++ src/utils/logUnexpectedError.ts
@@ -1,9 +1,38 @@
+import * as colors from 'colors/safe'
+
+function colored(str: string) {
+	if (!str || str.length < 2) return ''
+	return colors.red(str)
+}
+
+function bold(str: string) {
+	if (!str || str.length < 2) return ''
+	return colors.bold(str)
+}
+
+function black(str: string) {
+	if (!str || str.length < 2) return ''
+	return colors.black(str)
+}
+
+function hr() {
+	return colored(bold('-----------------'))
+}
+
+function h1(str: string) {
+	if (!str || str.length < 2) return ''
+	return hr() + '\n' + colored(bold(str))
+}
+
+function h2(str: string) {
+	if (!str || str.length < 2) return ''
+	return hr() + '\n' + black(str)
+}
+
 export function logUnexpectedError(err: Error) {
-	console.error('-----------------')
-	console.error('UNEXPECTED ERROR')
-	console.error('-----------------')
-	console.error(err.message)
-	console.error('-----------------')
-	console.error(err)
-	console.error('-----------------')
+	console.log(
+		h1('UNEXPECTED ERROR' + '\n') + colored(err.message + '\n') + hr() + '\n',
+		err,
+		'\n' + hr()
+	)
 }
